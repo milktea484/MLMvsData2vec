a662eb0b30e3510f90b8f486d60d4b3278c7726d
nakmaura <kmtrmkn8011@icloud.com>
2026-04-02 02:51:23 +0900

    once complete

--- SSpredictor/test.py
+++ SSpredictor/test.py
@@ -4,20 +4,27 @@ import random
 import warnings
 from pathlib import Path
 
+import h5py
 import hydra
 import numpy as np
-import pretrain.models as PretrainModels
+import pandas as pd
 import torch
 from conf.config import MainConfig as TrainMainConfig
 from conf.test_config import MainConfig
 from dataset import create_dataloader
-from metrics import calculate_auc, update_confusion_matrix
+from metrics import calculate_auc, calculate_confusion_matrix
 from models import KnotFoldModel
 from modules import CosineScheduler
 from omegaconf import OmegaConf
 from pretrain.conf.config import MainConfig as PretrainMainConfig
+from pretrain.models import BaseModel as PretrainBaseModel
 from tqdm import tqdm
-from utils import get_embedding_dim, setup_test_config
+from utils import (
+    get_embedding_dim,
+    setup_test_config,
+    visualize_auc,
+    visualize_probability_matrix,
+)
 
 setup_test_config()
 
@@ -46,8 +53,26 @@ def main(cfg: MainConfig):
     if not train_model_path.exists():
         raise FileNotFoundError(f"Model path {train_model_path} does not exist.")
     
+    # 出力ディレクトリの作成
+    output_dir = train_model_path / "test_results" / f"{cfg.path.timestamp}"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    # logの設定
+    logging.basicConfig(
+        level=logging.INFO,  # Set the minimum log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+        format="%(asctime)s - %(name)s.%(lineno)d - %(levelname)s - %(message)s",
+        handlers=[
+            logging.StreamHandler(),  # Log to console
+            logging.FileHandler(output_dir / "log_test.txt", mode="w"),
+        ],
+    )
+    logger = logging.getLogger(__name__)
+    
+    # warningsの設定 (UserWarningを無視)
+    warnings.filterwarnings("ignore", category=UserWarning)
+    
     # 訓練済みモデルと対応するhydraconfigの読み込み
-    train_cfg_path = Path(cfg.path.output_dir) / cfg.path.timestamp / "train_config" / "config.yaml"
+    train_cfg_path = Path(cfg.path.output_dir) / cfg.SStrain_model_path.timestamp / "train_config" / "config.yaml"
     if not train_cfg_path.exists():
         raise FileNotFoundError(f"Train config path {train_cfg_path} does not exist.")
     
@@ -55,24 +80,20 @@ def main(cfg: MainConfig):
     
     # 設定の上書き
     train_cfg.dataset.test_file = cfg.dataset.test_file
+    
     if cfg.common.iterations is None:
         cfg.common.iterations = train_cfg.common.iterations
-    if cfg.experiment.kf_lambda is None:
-        cfg.experiment.kf_lambda = [train_cfg.model.kf_lambda]
-
-    # logの設定
-    logging.basicConfig(
-        level=logging.INFO,  # Set the minimum log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-        format="%(asctime)s - %(name)s.%(lineno)d - %(levelname)s - %(message)s",
-        handlers=[
-            logging.StreamHandler(),  # Log to console
-            logging.FileHandler(train_model_path / "log_test.txt", mode="w"),
-        ],
-    )
-    logger = logging.getLogger(__name__)
+    else:
+        if cfg.common.iterations > train_cfg.common.iterations:
+            raise ValueError(f"Test config iterations {cfg.common.iterations} cannot be greater than train config iterations {train_cfg.common.iterations}.")
     
-    # warningsの設定 (UserWarningを無視)
-    warnings.filterwarnings("ignore", category=UserWarning)
+    kf_lambda_list = None
+    if train_cfg.model.name == "knotfold":
+        if cfg.experiment.kf_lambda_cfg.max is None or cfg.experiment.kf_lambda_cfg.min is None or cfg.experiment.kf_lambda_cfg.step is None:
+            logger.info("Kf_lambda_cfg is not fully specified. Will use kf_lambda from train config.")
+            kf_lambda_list = [train_cfg.model.kf_lambda]
+        else:
+            kf_lambda_list = np.arange(cfg.experiment.kf_lambda_cfg.min, cfg.experiment.kf_lambda_cfg.max + cfg.experiment.kf_lambda_cfg.step, cfg.experiment.kf_lambda_cfg.step).tolist()
 
     # 使用デバイスの設定
     if torch.cuda.is_available() and cfg.common.use_gpu:
@@ -103,7 +124,7 @@ def main(cfg: MainConfig):
         pretrain_cfg_path = pretrain_model_path / f"train_config/.hydra/config.yaml"
         pretrain_cfg: PretrainMainConfig = OmegaConf.load(pretrain_cfg_path)
         
-        pretrain_model: PretrainModels.BaseModel = hydra.utils.instantiate(
+        pretrain_model: PretrainBaseModel = hydra.utils.instantiate(
             pretrain_cfg.framework,
             padding_idx=pretrain_cfg.dataset.tokens.index("<pad>"),
             num_tokens=len(pretrain_cfg.dataset.tokens),
@@ -147,14 +168,6 @@ def main(cfg: MainConfig):
     models = []
     if train_cfg.model.name == "knotfold":
         ref_models = []
-        
-    # ROCやPRを用いた評価をするための混合行列. 各要素はサイズlen(thresholds)のnumpy配列になる
-    confusion_dict = {
-        "tp": 0,
-        "tn": 0,
-        "fp": 0,
-        "fn": 0,
-    }
     
     for iteration in range(cfg.common.iterations):
         logger.info(f"--------Iteration {iteration + 1}/{cfg.common.iterations}--------")
@@ -167,6 +180,7 @@ def main(cfg: MainConfig):
             use_attention=train_cfg.experiment.use_attention,
             device=device
         )
+        model.eval()
         
         # 訓練済みモデルの重みの読み込み
         model._load_state_dict(torch.load(train_model_path / f"weights/prior_{iteration}.pth", map_location=device))
@@ -188,17 +202,38 @@ def main(cfg: MainConfig):
             ref_models.append(copy.deepcopy(ref_model))
 
     # 全体の結果の保存
-    overall_results = {
-        "y_probs": [],
-        "test_losses": [],
+    overall_results = {"test_losses": []}
+    if train_cfg.model.name == "knotfold":
+        overall_results["ref_test_losses"] = []
+    
+    # probability_matrixの保存用
+    if cfg.common.save_probability_matrix:
+        probability_matrix_path = output_dir / "predicted_probability_matrices.h5"
+        hdf5_file = h5py.File(probability_matrix_path, "w")
+    
+    # ROCやPRを用いた評価をするための混合行列. 各要素はサイズlen(thresholds)のnumpy配列になる
+    confusion_dict = {
+        "tp": 0,
+        "tn": 0,
+        "fp": 0,
+        "fn": 0,
     }
+    
+    # predictの引数の用意
+    # cpuメモリに保存することになるので, メモリエラーに注意
+    predict_args = {
+        "batch_list": [],
+    }
+    
+    if train_cfg.model.name == "knotfold":
+        predict_args["kf_lambda_list"] = kf_lambda_list
 
     # バッチごとに処理
-    for batch in tqdm(test_loader, desc="Testing"):
+    for i, batch in enumerate(tqdm(test_loader, desc="Testing")):
         
         # iterationごとの結果を保存するための変数
         iter_results = {
-            "y_probs": [],
+            "logits": [],
             "test_losses": [],
         }
         if train_cfg.model.name == "knotfold":
@@ -209,57 +244,143 @@ def main(cfg: MainConfig):
             # iterationごとにモデルのテストを実行
             for iteration in range(cfg.common.iterations):
                 outputs = models[iteration]._test(batch)
-                iter_results["y_probs"].append(outputs["y_prob"])
+                iter_results["logits"].append(outputs["logits"])
                 iter_results["test_losses"].append(outputs["loss"])
                 
                 if train_cfg.model.name == "knotfold":
                     ref_outputs = ref_models[iteration]._test(batch)
-                    iter_results["ref_y_probs"].append(ref_outputs["y_prob"])
+                    iter_results["ref_logits"].append(ref_outputs["logits"])
                     iter_results["ref_test_losses"].append(ref_outputs["loss"])
             
             # iterationごとの予測確率と損失を平均
-            mean_y_prob = torch.mean(torch.stack(iter_results["y_probs"], dim=0), dim=0) # (iterations, B, L, L) -> (B, L, L)
+            mean_logits = torch.mean(torch.stack(iter_results["logits"], dim=0), dim=0) # (iterations, B, L, L) -> (B, L, L)
             mean_test_loss = torch.mean(torch.stack(iter_results["test_losses"], dim=0))
-            overall_results["y_probs"].append(mean_y_prob.detach().cpu())
-            overall_results["test_losses"].append(mean_test_loss.detach().cpu().item())
             
             if train_cfg.model.name == "knotfold":
-                mean_ref_y_prob = torch.mean(torch.stack(iter_results["ref_y_probs"], dim=0), dim=0) # (iterations, B, L, L) -> (B, L, L)
+                mean_ref_logits = torch.mean(torch.stack(iter_results["ref_logits"], dim=0), dim=0) # (iterations, B, L, L) -> (B, L, L)
                 mean_ref_test_loss = torch.mean(torch.stack(iter_results["ref_test_losses"], dim=0))
                 # overall_results["ref_y_probs"].append(mean_ref_y_prob.detach().cpu())
-                # overall_results["ref_test_losses"].append(mean_ref_test_loss.detach().cpu().item())
+                overall_results["ref_test_losses"].append(mean_ref_test_loss.detach().cpu().item())
             
             # 評価と予測
-            for batch_idx, length, seq_id, sequence, gt_bp_matrix, pred_bp_matrix in enumerate(zip(batch["lengths"], batch["seq_ids"], batch["sequences"], batch["bp_matrices"], mean_y_prob)):
+            for batch_idx, seq_id, sequence, length, gt_bp_matrix, pred_bp_prob in enumerate(zip(batch["seq_ids"], batch["sequences"], batch["lengths"], batch["bp_matrices"], mean_logits)):
                 # paddingの除去
                 gt_bp_matrix = gt_bp_matrix[:length, :length]
-                pred_bp_matrix = pred_bp_matrix[:length, :length]
+                pred_bp_prob = pred_bp_prob[:length, :length]
                 
-                # pred_bp_matrixはロジットなのでシグモイド関数を通して確率に変換
-                pred_bp_matrix = torch.sigmoid(pred_bp_matrix)
+                # pred_bp_probはロジットなのでシグモイド関数を通して確率に変換
+                pred_bp_prob = torch.sigmoid(pred_bp_prob)
+
+                # 全体の結果の保存
+                overall_results["test_losses"].append(mean_test_loss.detach().cpu().item())
+                if cfg.common.save_probability_matrix:
+                    hdf5_file.create_dataset(seq_id, data=pred_bp_prob.detach().cpu().numpy())
+                    
+                # 初回ならばシーケンスごとの予測確率行列を可視化して保存
+                if i == 0:
+                    probability_matrix_path = output_dir / f"{seq_id}_probability_matrix.png"
+                    visualize_probability_matrix(gt_bp_matrix, pred_bp_prob, output_path=probability_matrix_path)
 
                 # 混合行列の更新
-                confusion_dict = update_confusion_matrix(gt_bp_matrix.cpu(), pred_bp_matrix.cpu(), step=cfg.evaluation.auc_step, confusion_dict=confusion_dict)
-                
-                # predictの引数の用意
-                predict_args = {
-                    "gt_bp_matrix": gt_bp_matrix,
-                    "pred_bp_matrix": pred_bp_matrix
-                }
+                if cfg.common.evaluation:
+                    batch_confusion_dict = calculate_confusion_matrix(gt_bp_matrix.cpu(), pred_bp_prob.cpu(), step=cfg.evaluation.auc_step)
+                    confusion_dict["tp"] += batch_confusion_dict["tp"]
+                    confusion_dict["tn"] += batch_confusion_dict["tn"]
+                    confusion_dict["fp"] += batch_confusion_dict["fp"]
+                    confusion_dict["fn"] += batch_confusion_dict["fn"]
 
-                # knotfoldの場合
-                if train_cfg.model.name == "knotfold":
-                    ref_bp_matrix = mean_ref_y_prob[batch_idx][:length, :length]
-                    ref_bp_matrix = torch.sigmoid(ref_bp_matrix)
-                    predict_args["ref_bp_matrix"] = ref_bp_matrix
-                    predict_args["kf_lambda"] = cfg.experiment.kf_lambda
-                
-                # 予測 (ここからつづき)
-                model.predict(**predict_args)
-            
-            
-    # 全体のROC AUCやPR AUCの計算
-    figure_materials, auc_dict = calculate_auc(confusion_dict)
+                # predictの引数の用意
+                if cfg.common.prediction:
+                    predict_args_batch = {
+                        "seq_id": seq_id,
+                        "sequence": sequence,
+                        "length": length,
+                        "gt_bp_matrix": gt_bp_matrix, # (L, L)
+                        "pred_bp_prob": pred_bp_prob, # (L, L)
+                    }
+
+                    # knotfoldの場合
+                    if train_cfg.model.name == "knotfold":
+                        ref_bp_prob = mean_ref_logits[batch_idx][:length, :length]
+                        ref_bp_prob = torch.sigmoid(ref_bp_prob)
+                        predict_args_batch["ref_bp_prob"] = ref_bp_prob # (L, L)
+                        
+                    predict_args["batch_list"].append(predict_args_batch)
     
-    overall_results["roc_auc"] = auc_dict["roc_auc"]
-    overall_results["pr_auc"] = auc_dict["pr_auc"]
+    overall_results["test_losses"] = np.mean(overall_results["test_losses"])
+    if train_cfg.model.name == "knotfold":
+        overall_results["ref_test_losses"] = np.mean(overall_results["ref_test_losses"])
+    
+    # probability_matrixの保存を閉じる
+    if cfg.common.save_probability_matrix:
+        hdf5_file.close()
+    
+    # 評価
+    if cfg.common.evaluation:
+        # 全体のROC AUCやPR AUCの計算
+        figure_materials, auc_dict = calculate_auc(confusion_dict)
+        auc_figure_path = output_dir / "auc_curve.png"
+        
+        # ROC曲線とPR曲線の描画
+        visualize_auc(figure_materials, auc_dict, output_path=auc_figure_path)
+        logger.info(f"Saved ROC and PR curve figure to {auc_figure_path}")
+        overall_results["roc_auc"] = auc_dict["roc_auc"]
+        overall_results["pr_auc"] = auc_dict["pr_auc"]
+        logger.info(f"ROC AUC: {auc_dict['roc_auc']:.4f}, PR AUC: {auc_dict['pr_auc']:.4f}")
+    
+    # 予測
+    if cfg.common.prediction:
+        results = model.predict(**predict_args)
+        
+        # 結果の保存
+        ## knotfoldのkf_lambdaごとの結果の保存
+        if train_cfg.model.name == "knotfold":
+            # kf_lambda_resultsをcsvに保存
+            kf_lambda_results = results["kf_lambda_results"]
+            kf_lambda_results_list = []
+            for kf_lambda, scores in kf_lambda_results.items():
+                kf_lambda_results_list.append({
+                    "kf_lambda": kf_lambda,
+                    "is_optimal": scores["is_optimal"],
+                    "precision": scores["precision"],
+                    "recall": scores["recall"],
+                    "f1": scores["f1"],
+                })
+            kf_lambda_results_path = output_dir / "kf_lambda_results.csv"
+            kf_lambda_results_df = pd.DataFrame(kf_lambda_results_list)
+            kf_lambda_results_df.to_csv(kf_lambda_results_path, index=False)
+            logger.info(f"Saved kf_lambda results to {kf_lambda_results_path}")
+            
+            #is_optimalがTrueのスコアをoverall_resultsに保存
+            optimal_kf_lambda_results = kf_lambda_results_df[kf_lambda_results_df["is_optimal"] == True]
+            if len(optimal_kf_lambda_results) > 0:
+                overall_results["kf_lambda"] = optimal_kf_lambda_results.iloc[0]["kf_lambda"]
+                overall_results["precision"] = optimal_kf_lambda_results.iloc[0]["precision"]
+                overall_results["recall"] = optimal_kf_lambda_results.iloc[0]["recall"]
+                overall_results["f1"] = optimal_kf_lambda_results.iloc[0]["f1"]
+        
+        ## 予測結果の保存
+        prediction_to_csv_info = []
+        for pred_result in results["prediction_results"]:
+            pred_result_info = {
+                "id": pred_result["seq_id"],
+                "sequence": pred_result["sequence"],
+                "base_pairs": pred_result["pairs"],
+                "len": pred_result["length"],
+                "precision": pred_result["scores"]["precision"],
+                "recall": pred_result["scores"]["recall"],
+                "f1": pred_result["scores"]["f1"],
+            }
+            prediction_to_csv_info.append(pred_result_info)
+        
+        prediction_results_path = output_dir / "prediction_results.csv"
+        pd.DataFrame(prediction_to_csv_info).to_csv(prediction_results_path, index=False)
+        logger.info(f"Saved prediction results to {prediction_results_path}")
+        
+        ## 全体の結果の保存
+        overall_results_path = output_dir / "overall_results.csv"
+        pd.DataFrame([overall_results]).to_csv(overall_results_path, index=False)
+        logger.info(f"Saved overall results to {overall_results_path}")
+            
+if __name__ == "__main__":
+    main()
