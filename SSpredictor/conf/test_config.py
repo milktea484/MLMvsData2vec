07592bcfadd52556cf17858c573d711716ce4baa
nakmaura <kmtrmkn8011@icloud.com>
2026-04-02 02:48:13 +0900

    add CommonConfig and KfLmabdaConfig, and some fix

--- SSpredictor/conf/test_config.py
+++ SSpredictor/conf/test_config.py
@@ -9,15 +9,25 @@ class CommonConfig:
     seed: int = MISSING
     batch_size: int = MISSING
     use_gpu: bool = MISSING
-    iterations: int = None
+    iterations: int | None = None
     """デフォルトはtrain_configの設定に従う"""
+    evaluation: bool = MISSING
+    """評価を行うかどうか. デフォルトはTrue"""
+    prediction: bool = MISSING
+    """予測を行うかどうか. デフォルトはTrue"""
+    save_probability_matrix: bool = MISSING
+    """予測された塩基対確率行列を保存するかどうか. デフォルトはTrue"""
+    
+    def __post_init__(self):
+        if self.evaluation is False and self.prediction is False:
+            raise ValueError("At least one of evaluation or prediction must be True.")
 
 @dataclass
 class PretrainConfig:
-    framework: str = None
+    framework: str | None = None
     """使用する事前学習モデルのフレームワーク"""
     
-    timestamp: str = None
+    timestamp: str | None = None
     """使用する事前学習モデルのタイムスタンプ"""
 
 @dataclass
@@ -32,20 +42,29 @@ class SStrainModelPathConfig:
     
 @dataclass
 class DatasetConfig:
-    embedding_file: str = None
+    embedding_file: str | None = None
     test_file: str = MISSING
     
 @dataclass
 class EvaluationConfig:
     auc_step: float = MISSING
     """ROC AUCとPR AUCを計算する際の閾値の刻み幅. 例えば0.01なら0, 0.01, 0.02, ..., 1の100点で評価することになる"""
-    
+
+@dataclass
+class KfLambdaConfig:
+    min: float | None = None
+    """kf_lambdaの最小値"""
+    max: float | None = None
+    """kf_lambdaの最大値"""
+    step: float | None = None
+    """kf_lambdaの刻み幅"""
+
 @dataclass
 class ExperimentConfig:
     name: str = MISSING
-    additional_experiment_info: str = None
-    kf_lambda: list[float] = MISSING
-    """KnotFoldのmin cost flowアルゴリズムで使用するλのリスト. デフォルトはtrain_configの設定に従う"""
+    additional_experiment_info: str | None = None
+    kf_lambda_cfg: KfLambdaConfig = MISSING
+    """KnotFoldの最小フローアルゴリズムのlambdaの設定. minからmaxまでstep刻みで複数のkf_lambdaを試す. デフォルトはtrain_config(knotfold.yaml)の設定に従う"""
     
 @dataclass
 class MainConfig:
